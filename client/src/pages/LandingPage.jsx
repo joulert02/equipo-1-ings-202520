a09e9f7eca9ce01d4105329f682dc15194803000
Jouler Talaigua <50017852+joulert02@users.noreply.github.com>
2025-10-23 17:10:53 -0500

    feat: add new copy in landing

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -27,7 +27,7 @@ const LandingPage = () => {
           lineHeight: '1.2',
           marginBottom: '76px'
         }}>
-          Consulta precios de la canasta familiar colombiana en tiempo real. en dev 2
+          Consulta precios de la canasta familiar colombiana en tiempo real. Estamos en la UPB
         </h1>
 
         {/* Hero section */}
